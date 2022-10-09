80b97dec467d301817dc6fb1147ce4db62b4b4ea
jace.jeong <jace.jeong@blue-dot.io>
2022-10-09 23:38:58 +0900

    setting run_pti path

--- scripts/run_pti.py
+++ scripts/run_pti.py
@@ -1,3 +1,7 @@
+import os
+import sys
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+
 from random import choice
 from string import ascii_uppercase
 from torch.utils.data import DataLoader
